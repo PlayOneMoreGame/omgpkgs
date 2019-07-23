87d4366c9e76f12adeec0177a03db2ee12389cac
Jamie Winsor <jamie@onemoregame.com>
2019-07-23 17:50:47 +0200

    Add Buildkite pipeline to project
    Signed-off-by: Jamie Winsor <jamie@onemoregame.com>

--- default.nix
+++ default.nix
@@ -1,5 +1,14 @@
-{ pkgs ? import <nixpkgs> {} }:
+let
+  # Look here for information about how to generate `nixpkgs-version.json`.
+  #  → https://nixos.wiki/wiki/FAQ/Pinning_Nixpkgs
+  pinned-version = builtins.fromJSON (builtins.readFile ./.nixpkgs-version.json);
+  pinned-nixpkgs = import (builtins.fetchGit {
+    inherit (pinned-version.nixpkgs) url rev;
+    ref = "nixos-unstable";
+  }) {};
+in
 
+{ pkgs ? pinned-nixpkgs }:
 with pkgs;
 {
   nodePackages_10_x = recurseIntoAttrs (callPackage ./development/node-packages/default-v10.nix {
