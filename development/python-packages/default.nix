e9995be65df30be4e42a91ff3f26a583002a4e63
Jamie Winsor <jamie@onemoregame.com>
2020-01-07 17:47:09 -0800

    Disable amicleaner package
    Signed-off-by: Jamie Winsor <jamie@onemoregame.com>

--- development/python-packages/default.nix
+++ development/python-packages/default.nix
@@ -3,5 +3,8 @@
 with pkgs;
 rec {
   argparse = python3Packages.callPackage ./argparse.nix {};
-  aws-amicleaner = callPackage ./aws-amicleaner.nix { inherit argparse awscli; };
+  # JW TODO: Fix ami-cleaner package to not be part of the python3
+  # installation and instead behave like awscli which is a standalone
+  # application backed by Python.
+  # aws-amicleaner = callPackage ./aws-amicleaner.nix { inherit argparse awscli; };
 }
