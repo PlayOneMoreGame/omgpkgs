aa464be3005bcce1f47a59c7a827d7ce747bae3d
Jamie Winsor <jamie@onemoregame.com>
2020-01-07 17:09:51 -0800

    Remove unused and outdated Python packages
    Signed-off-by: Jamie Winsor <jamie@onemoregame.com>

--- development/python-packages/default.nix
+++ development/python-packages/default.nix
@@ -1,10 +1,7 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs }:
 
 with pkgs;
 rec {
   argparse = python3Packages.callPackage ./argparse.nix {};
-  awscli = callPackage ./awscli.nix {};
   aws-amicleaner = callPackage ./aws-amicleaner.nix { inherit argparse awscli; };
-  # colorama-3 = python3Packages.callPackage ./colorama-3.nix {};
-  # rsa = python3Packages.callPackage ./rsa.nix {};
 }
