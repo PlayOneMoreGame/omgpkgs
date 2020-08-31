f8f09b76f2ae9387281efe74236e129b1f610585
Jamie Winsor <jamie@onemoregame.com>
2020-08-31 12:13:54 -0700

    Fix aws amicleaner package implementation
    Signed-off-by: Jamie Winsor <jamie@onemoregame.com>

--- development/python-packages/default.nix
+++ development/python-packages/default.nix
@@ -2,8 +2,4 @@
 
 with pkgs; rec {
   argparse = python3Packages.callPackage ./argparse.nix { };
-  # JW TODO: Fix ami-cleaner package to not be part of the python3
-  # installation and instead behave like awscli which is a standalone
-  # application backed by Python.
-  # aws-amicleaner = callPackage ./aws-amicleaner.nix { inherit argparse awscli; };
 }
